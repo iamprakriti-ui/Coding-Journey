f8e86eb062518a00a2dec050274820d5c929352b
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:37:28 +0530

    Add whole number variable practice

---
+++ whole_numbers.c
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize variables here
+     int quantity ;
+     quantity = 5   ; 
+    
+    // Output the values - Don't change below this line
+    std::cout << "Quantity: " << quantity;
+    
+    return 0;
+}
