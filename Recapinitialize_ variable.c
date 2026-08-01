f12d37505964ae9f10be4a76f03d35a580424f15
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 15:38:12 +0530

    Initialize variables and print their values

---
+++ Recapinitialize_ variable.c
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Type your code below
+    int k= 88;
+    float PI= 3.14;
+    std:: string name="bob";
+
+    
+    // Don't change the lines below
+    std::cout << "k = " << k << std::endl;
+    std::cout << "PI = " << PI << std::endl;
+    std::cout << "name = " << name << std::endl;
+    return 0;
