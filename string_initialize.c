6c1a3af69b7603c2f950e9ac5417862a7467c14b
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:41:59 +0530

    Add string initialization and output example

---
+++ string_initialize.c
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declare and initialize variables here
+    string coddy  = "I am learning to code with Coddy" ;
+    
+    
+    // Output the values
+    std::cout << "Coddy = \"" << coddy << "\"" <<std::endl;
+
+    return 0;
+}
