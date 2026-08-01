868d777610cf983bb845a810ea1637632d02ea77
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:39:55 +0530

    Add real_numbers.c to display price and temperature

---
+++ real_numbers.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize variables here
+    float itemPrice = 24.99f;
+    double temperature = 23.5 ;
+    
+    
+    
+    // Output the values
+    std::cout << "Price: " << itemPrice << std::endl;
+    std::cout << "Temperature: " << temperature << std::endl;
+    
+    return 0;
+}
