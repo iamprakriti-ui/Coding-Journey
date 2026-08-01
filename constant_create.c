429102ffdb28bd3773c2a132e0c5f90848e7f450
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:46:17 +0530

    Add constant PI to constant_create.c

---
+++ constant_create.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+ const double  PI = 3.14159;
+
+    // Don't change the line below
+    std::cout << "PI = " << PI;
+    return 0;
+}
