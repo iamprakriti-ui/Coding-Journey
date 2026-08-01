bf0cf969d53d4c23436ace1eb48af534f022e6f9
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:43:26 +0530

    Declare boolean variable isLoggedIn and output value

---
+++ boolean_declare.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+    bool isLoggedIn = true;
+    
+    // Don\'t change the line below
+    std::cout << "isLoggedIn = " << isLoggedIn;
+    return 0;
+}
