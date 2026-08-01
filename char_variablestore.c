8abd15299b66e584615bb34eaeaad62eead011aa
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:44:28 +0530

    Add char variable example in char_variablestore.c

---
+++ char_variablestore.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Type your code below
+    char initial = 'B';
+    
+    // Don't change the line below
+    std::cout << "initial = '" << initial << "'";
+    return 0;
+}
