4589f9c15039ae4d35752c2118bbe255f7387933
Prakriti <prakritisingh14052008@gmail.com>
2026-08-01 14:56:50 +0530

    Add type declaration example in C++

---
+++ type_dec;aration.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Declaring variables
+    int count;
+    count= 10;
+    double total;
+    total= 150.75;
+    char grade;
+    grade= 'A';
+     bool isActive;
+    isActive: false;
+     string userName = "Bob123";
+
+
+    // Printing values
+    cout << "Count: " << count << endl;
+    cout << "Total: " << total << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Active: " << isActive << endl;
+    cout << "User Name: " << userName << endl;
+
+    return 0;
+}
