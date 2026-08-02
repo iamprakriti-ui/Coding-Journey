859d9070c213cd2c8aeb63e2ed0a5757603072d1
Prakriti <prakritisingh14052008@gmail.com>
2026-08-02 19:52:27 +0530

    Add modulo operators example in modulo_operators.py

---
+++ Python/Variables/modulo_operators.py
@@ -0,0 +1,15 @@
+# Type your code below
+a=9
+b=2
+c=11
+d=a%2
+e=b%3
+f=c%10
+
+# Don't change the line below
+print(f"a = {a}")
+print(f"b = {b}")
+print(f"c = {c}")
+print(f"d = {d}")
+print(f"e = {e}")
+print(f"f = {f}")
