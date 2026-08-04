5b6d021a9557425a1e0c4c95ed0624c5567922b8
Prakriti <prakritisingh14052008@gmail.com>
2026-08-04 14:18:48 +0530

    Add typecasting example in typecasting1.cpp
    This program demonstrates typecasting from double to int.

---
+++ C++/Variables/typecasting1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    // Declare and initialize variables
+    double price = 99.99;
+    int intPrice = 99; // Explicit casting from double to int
+    
+    
+    // Output the values
+    std::cout << "Price: " << price << std::endl;
+    std::cout << "Int Price: " << intPrice << std::endl;
+    
+    return 0;
+}
